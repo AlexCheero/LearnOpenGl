0600be7193046fdbbbe22b0ac50c128278dd1b6f
alexcheero157 <alexcheero157@gmail.com>
2018-12-27 22:32:57 +0300

    addition to prev commit

--- LearnOpenGL/Main.cpp
+++ LearnOpenGL/Main.cpp
@@ -158,10 +158,10 @@ int main(int argc, char *argv[])
 	unsigned int VAO = PrepareData(
 		{
 			// positions         // colors           // texture coords
-			0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f,   // top right
-			0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f,   // bottom right
+			0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+			0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
 		   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-		   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f    // top left 
+		   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 		},
 		{
 			0, 1, 3, // first triangle
