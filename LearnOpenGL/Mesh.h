29679893ffdcb2361c61fb91efca0e93d9e022b7
ACherkasov <ACherkasov@spb.luxoft.com>
2019-01-30 20:08:56 +0300

    Mesh class

---
+++ LearnOpenGL/Mesh.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#include <glm/glm.hpp>
+
+#include "Shader.h"
+
+struct Vertex
+{
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec3 TexCoords;
+};
+
+struct Texture
+{
+	unsigned int id;
+	std::string type;
+};
+
+class Mesh
+{
+	unsigned int VAO;
+	unsigned int VBO;
+	unsigned int EBO;
+
+	void SetupMesh();
+public:
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+
+	Mesh(std::vector<Vertex> verts, std::vector<unsigned int> inds, std::vector<Texture> txts) : vertices(verts), indices(inds), textures(txts)
+	{
+		SetupMesh();
+	}
+
+	void Draw(Shader shader);
+};
+
